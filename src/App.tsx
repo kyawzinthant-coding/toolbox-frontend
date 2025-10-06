7087f0e094da99d8af4c57d4fc39226fce3353a3
kyawzinthant-coding <kyawthantver2@gmail.com>
2025-10-06 18:11:43 +0630

    :sparkles: feat(init): init the project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="font-bold text-lg">Welcome to dev toolbox</h1>
+    </div>
+  );
+}
+
+export default App;
